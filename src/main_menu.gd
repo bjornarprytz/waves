8be9f86790bb3f7f68f50b08fbae8a18e6e2da7c
Bjørnar Prytz <bjornarprytz@gmail.com>
2025-11-25 06:54:50 +0100

    world menu

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -18,16 +18,38 @@ class Settings:
 	
 		self.starting_lives = lives
 
-@onready var game_mode_button_group: ButtonGroup = %Short.button_group
+var modes = ["Short", "Long", "Endless"]
 
-func _input(event: InputEvent) -> void:
-	if (event.is_action_pressed("start")):
-		_on_start_button_pressed()
+@onready var mode_left: Button = %ModeLeft
+@onready var mode_right: Button = %ModeRight
+@onready var mode_name: RichTextLabel = %ModeName
+
+var current_mode_idx: int = 0
 
 func _on_start_button_pressed() -> void:
-	var current_mode = game_mode_button_group.get_pressed_button()
-	var mode_name = "Endless"
-	if (current_mode):
-		mode_name = current_mode.name
+	var current_mode = modes[current_mode_idx]
 	
-	Events.start_game.emit(Settings.new(mode_name))
+	Events.start_game.emit(Settings.new(current_mode))
+
+
+func _input(event: InputEvent) -> void:
+	if (event is InputEventKey and event.pressed):
+		if (event.keycode == Key.KEY_LEFT):
+			_on_mode_left_pressed()
+		elif (event.keycode == Key.KEY_RIGHT):
+			_on_mode_right_pressed()
+		elif (event.keycode == Key.KEY_ENTER or event.keycode == Key.KEY_KP_ENTER):
+			_on_start_button_pressed()
+
+
+func _on_mode_left_pressed() -> void:
+	current_mode_idx = (current_mode_idx - 1) % modes.size()
+	_update_mode_name()
+
+func _on_mode_right_pressed() -> void:
+	current_mode_idx = (current_mode_idx + 1) % modes.size()
+	_update_mode_name()
+
+func _update_mode_name() -> void:
+	mode_name.clear()
+	mode_name.append_text(modes[current_mode_idx])
